b3892d455662743aa634bc0e56a5d23278aa6a42
Berezkin Artem <Berezkin Artem@DESKTOP-J41KGDU>
2025-06-07 23:49:37 +0500

    Task5

---
+++ Tyuiu.AntonovVV.Sprint4.Task5.V9.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.AntonovVV.Sprint4.Task5.V9.Lib;
+namespace Tyuiu.AntonovVV.Sprint4.Task5.V9.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] mas2 = new int[5, 5] { { -2, 2, -4, 1, 0 }, { 0, -2, 0, -4, -4 }, { 1, -1, 4, -4, -4 }, { 3, 1, 0, 4, -1 }, { -3, 3, -2, -1, 3 } };
+            int res = ds.Calculate(mas2);
+            int wait = 9;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
